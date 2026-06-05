7a9158294808164c109ca9d6b411ec830c7aacf3
Yashwanthpatil2005 <yashwanthp878@gmail.com>
2026-06-05 23:30:49 +0530

    written a code for reverse string

---
+++ reversestring.py
@@ -0,0 +1,10 @@
+# Reverse a String using Loop
+
+text = input("Enter a string: ")
+
+reversed_text = ""
+
+for char in text:
+    reversed_text = char + reversed_text
+
+print("Reversed string:", reversed_text)
