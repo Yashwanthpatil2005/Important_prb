72638182a283e3aeed6cdece0703dff95e7cd91b
Yashwanthpatil2005 <yashwanthp878@gmail.com>
2026-06-09 21:54:50 +0530

    fiding missing number in array

---
+++ missingnumber.py
@@ -0,0 +1,12 @@
+# Find Missing Number
+
+arr = [1, 2, 3, 5]
+
+n = 5
+
+expected_sum = n * (n + 1) // 2
+actual_sum = sum(arr)
+
+missing_number = expected_sum - actual_sum
+
+print("Missing Number:", missing_number)
