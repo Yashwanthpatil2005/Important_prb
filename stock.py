9fb2b563582f4e642c8154f863c35abc65522f5c
Yashwanthpatil2005 <yashwanthp878@gmail.com>
2026-06-10 23:11:46 +0530

    Create stock.py

---
+++ stock.py
@@ -0,0 +1,18 @@
+# Stock Buy & Sell
+
+prices = [7, 1, 5, 3, 6, 4]
+
+min_price = prices[0]
+max_profit = 0
+
+for price in prices:
+
+    if price < min_price:
+        min_price = price
+
+    profit = price - min_price
+
+    if profit > max_profit:
+        max_profit = profit
+
+print("Maximum Profit:", max_profit)
