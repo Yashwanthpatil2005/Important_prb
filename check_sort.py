8882ee5d767f1a509a695d975c1c26597606586a
Yashwanthpatil2005 <yashwanthp878@gmail.com>
2026-08-28 10:49:20 +0530

    Add function to check if an array is sorted

---
+++ check_sort.py
@@ -0,0 +1,15 @@
+# Check if Array is Sorted
+
+arr = [1, 2, 3, 4, 5]
+
+is_sorted = True
+
+for i in range(len(arr) - 1):
+    if arr[i] > arr[i + 1]:
+        is_sorted = False
+        break
+
+if is_sorted:
+    print("Array is sorted")
+else:
+    print("Array is not sorted")
