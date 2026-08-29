b67d0c8caec05dea437ae630b36e0732ae9ef081
Yashwanthpatil2005 <yashwanthp878@gmail.com>
2026-08-29 21:04:25 +0530

    Add script to remove duplicates from an array

---
+++ remove_duplicate.py
@@ -0,0 +1,12 @@
+# Remove Duplicates
+
+arr = [1, 2, 2, 3, 4, 4, 5]
+
+result = []
+
+for num in arr:
+    if num not in result:
+        result.append(num)
+
+print("Original Array:", arr)
+print("After Removing Duplicates:", result)
